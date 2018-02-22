14020ece5f174838625cd2dc2dd3765f03edd030
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2018-02-22 13:14:12 -0300

    Basic GET

---
+++ scripts/NancyTests/NancyTests/CarModule.cs
@@ -0,0 +1,13 @@
+﻿using Nancy;
+
+namespace NancyTests
+{
+    public class CarModule: NancyModule
+    {
+        public CarModule()
+        {
+            //Adding a simple route
+            Get["/status"] = _ => "Hello World";
+        }
+    }
+}
